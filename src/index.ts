8021c740f4f1ce68869d53494a2fd2eb957c7bb6
mark1ns0n <6otan1dze@gmail.com>
2019-10-15 21:31:58 +0300

    add support express

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,19 @@
 import Telegraf from 'telegraf';
+import express, { Request, Response } from 'express';
+
+const app = express();
+app.use(express.json());
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.start((ctx: any) => ctx.reply('Welcome!'));
 
 bot.launch();
+
+app.post('/docker-service-status', (req: Request, res: Response) => {
+  console.log(req.body);
+  res.status(200).end();
+});
+
+app.listen(process.env.BOT_PORT, () => {
+  console.log(`Bot started at port ${process.env.BOT_PORT}`);
+});
